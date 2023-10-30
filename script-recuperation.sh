f4904fa77df503b2ea71b216f7065c6b2fa3e830
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2023-10-30 23:26:23 +0100

    Job 1 OK

---
+++ script-recuperation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Author: Cyril GENISSON
+# Created: 30/10/2023
+# Updated: 30/10/2023
+#
+# Nom du script: script-recuperation.sh
+# Description: 
+#
+
+# Activation du mode debug, à commenter quand le script est opérationnel.
+set -ux
+
---
+++ script-tshark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Author: Cyril GENISSON
+# Created: 30/10/2023
+# Updated: 30/10/2023
+#
+# Nom du script: script-tshark.sh
+# Description: 
+#
+
+# Activation du mode debug, à commenter quand le script est opérationnel.
+set -ux
+
