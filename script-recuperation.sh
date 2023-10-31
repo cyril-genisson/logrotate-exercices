3c1651935b1f804aa3489506eba1dfb1fd636df6
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2023-10-31 02:23:28 +0100

    Job 2 OK

--- script-recuperation.sh
+++ script-recuperation.sh
@@ -5,9 +5,35 @@
 # Updated: 30/10/2023
 #
 # Nom du script: script-recuperation.sh
-# Description: récupération du nombre de connexions journalier en fonction de deux date
+# Description: récupération du nombre de connexions journalier en fonction de deux dates jj-mm-yyyy   
 # example: ./script-recuperation.sh 08-SEP-2023 20-SEP-2023
 # sauvegarde: /var/backup/recuperation-30-OCT-2023.tar.gz
 
-SAVE=recuperation-$(date +"%d-%b-%Y").tar.gz
+SAVE=/var/backup/recuperation-$(date +"%d-%b-%Y").tar.gz
+AFTER=$1
+BEFORE=$2
+TEMP=/tmp/connexions
 
+if [ -d /tmp/connexions ]; then 
+    rm -fR $TEMP
+    mkdir $TEMP
+else
+    mkdir $TEMP
+fi
+
+function inverse {
+    day=$(echo $1 | cut -f1 -d '-')
+    month=$(echo $1 | cut -f2 -d '-')
+    year=$(echo $1 | cut -f3 -d '-')
+    echo "$year-$month-$day"
+}
+
+k=$(inverse $AFTER)
+while [ $k != $(inverse $BEFORE) ] ; do
+    journalctl --since $k --until $(date -d "$k + 1 days" +"%Y-%m-%d") | grep "New session" > $TEMP/connexions-$k
+    k=$(date -d "$k + 1 days" +"%Y-%m-%d")
+done
+journalctl --since $k --until $k | grep "New session" > /tmp/connexions-$k
+
+tar -czvf $SAVE $TEMP/connexions-*
+rm -f $TEMP/connexions-*
